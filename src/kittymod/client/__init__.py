70be97fd4b9215272415396c08c5fbe01d1207a4
eliseydudin <dudinelisey630@gmail.com>
2025-01-15 09:49:16 +0300

    more commands :3

--- src/kittymod/client/__init__.py
+++ src/kittymod/client/__init__.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 from loguru import logger
 from kittymod.database import Database
 from kittymod.config import get_config
@@ -16,13 +17,32 @@ class Client(commands.Cog):
 
     @commands.command("strike")
     async def strike(self, ctx: commands.Context, id: int):
-        strikes = self.db.add_strikes(id)
-        await ctx.reply(f"the user **{id}** currently got {strikes} strikes")
+        try:
+            user: discord.User = await ctx.bot.fetch_user(id)
+            strikes = self.db.add_strikes(id)
 
-        if strikes >= get_config()["MAX_STRIKES"]:
-            user = await ctx.bot.fetch_user(id)
-            print(user, type(user))
-            await ctx.message.guild.ban(user)
+            if strikes >= get_config()["MAX_STRIKES"]:
+                print(user, type(user))
+                await ctx.message.guild.ban(user)
+                await ctx.reply(
+                    f"the user **{user.display_name}** got banned for meeting the maximum amount of strikes\n"
+                    + f"their strikes: {strikes}"
+                )
+            else:
+                await ctx.reply(
+                    f"the user **{user.display_name}** currently got {strikes} strikes"
+                )
+
+        except Exception as e:
+            logger.error(f"error in strike: {e}")
             await ctx.reply(
-                f"the user **{id}** got banned for meeting the maximum amount of strikes"
+                "cannot fetch the user's data, are you sure they are in this server?"
             )
+
+    @commands.command("config")
+    async def config(self, ctx: commands.Context):
+        cfg = get_config()
+        text = f"max strikes for a user: {cfg["MAX_STRIKES"]}\n"
+
+        embed = discord.Embed(title="kittymod's config", description=text)
+        await ctx.reply(embed=embed)
